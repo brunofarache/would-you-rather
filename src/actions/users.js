fb69c0f7493bffed5688148b142300d8010c703b
Bruno Farache <bruno.farache@liferay.com>
2019-04-20 16:23:57 -0300

    source formatting

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 
-export function receiveUsers (users) {
+export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users
