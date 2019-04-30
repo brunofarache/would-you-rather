661e1556f5ec9f1a7055657535b51058ef4604be
Bruno Farache <bruno.farache@liferay.com>
2019-04-30 15:37:11 -0300

    change user answers too in store

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,22 @@
+import { ADD_VOTE } from '../actions/questions';
 import { RECEIVE_USERS } from '../actions/users';
 
 export default function users(state = {}, action) {
     switch (action.type) {
+        case ADD_VOTE:
+            const user = state[action.user];
+            const answers = user.answers;
+
+            return {
+                ...state,
+                [action.user]: {
+                    ...user,
+                    answers: {
+                        ...answers,
+                        [action.question]: action.option
+                    }
+                }
+            }
         case RECEIVE_USERS:
             return {
                 ...state,
