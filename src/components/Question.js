f0102ee3325c783af3f0db16b33716cd18eaf193
Bruno Farache <bruno.farache@liferay.com>
2019-04-30 11:30:32 -0300

    connect question to store

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Card, Form } from 'react-bootstrap';
 
 class Question extends Component {
 	render() {
-		const { question } = this.props;
+		const { author, question } = this.props;
 
 		return (
 			<Card style={{ width: '18rem' }}>
-				<Card.Header>{question.author} asks:</Card.Header>
+				<Card.Header>{author.name} asks:</Card.Header>
 				<Card.Body>
 					<Card.Title>Would You Rather?</Card.Title>
-					<Card.Text>
 					<Form>
-						<div className="mb-3">
-							<Form.Check 
-								type='radio'
-								id='one'
-								label={question.optionOne.text}
-							/>
-							<Form.Check 
-								type='radio'
-								id='two'
-								label={question.optionTwo.text}
-							/>
-						</div>
-						</Form>
-					</Card.Text>
+						<Form.Check 
+							type='radio'
+							name='answer'
+							id='one'
+							label={question.optionOne.text} />
+						<Form.Check 
+							type='radio'
+							name='answer'
+							id='two'
+							label={question.optionTwo.text} />
+					</Form>
 				</Card.Body>
 			</Card>
 		);
 	}
 }
 
-export default Question;
+function mapStateToProps({ questions, users }, { match }) {
+	const question = questions[match.params.id];
+	const author = users[question.author];
+
+	return {
+		question,
+		author
+	}
+}
+
+export default connect(mapStateToProps)(Question);
