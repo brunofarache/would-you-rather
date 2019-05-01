9853f9fe36de76e6e4924aee9d15840b228dc197
Bruno Farache <bruno.farache@liferay.com>
2019-04-30 23:34:56 -0300

    add created question to user as well

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,8 +1,18 @@
-import { ADD_VOTE } from '../actions/questions';
+import { ADD_QUESTION, ADD_VOTE } from '../actions/questions';
 import { RECEIVE_USERS } from '../actions/users';
 
 export default function users(state = {}, action) {
     switch (action.type) {
+        case ADD_QUESTION:
+            const author = state[action.question.author];
+
+            return {
+                ...state,
+                [action.question.author]: {
+                    ...author,
+                    questions: author.questions.concat(action.question.id)
+                }
+            };
         case ADD_VOTE:
             const user = state[action.user];
             const answers = user.answers;
