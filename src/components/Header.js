ec8c7fe2e508d433d907239ae0c804e7f0628ee4
Bruno Farache <bruno.farache@liferay.com>
2019-05-03 15:24:50 -0300

    use tabs

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,50 +6,50 @@ import { NavLink, withRouter } from 'react-router-dom'
 import { signOut } from '../actions/authedUser';
 
 class Header extends Component {
-    handleSignOut = () => {
-        const { dispatch, history } = this.props;
-        dispatch(signOut());
-        history.push('/');
-    }
+	handleSignOut = () => {
+		const { dispatch, history } = this.props;
+		dispatch(signOut());
+		history.push('/');
+	}
 
-    render() {
-        const { user } = this.props;
+	render() {
+		const { user } = this.props;
 
 		return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand>
-                    <Image
-                        src={user.avatarURL}
-                        className="d-inline-block align-top"
-                        style={{ height: '30px', marginRight: '8px'}} 
-                        roundedCircle />
-                        {`${user.name}`}
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <NavLink
-                            exact
-                            className="nav-link"
-                            to="/">
-                            Home
-                        </NavLink>
-                        <NavLink
-                            className="nav-link"
-                            to="/add">
-                            New Question
-                        </NavLink>
-                        <NavLink 
-                            className="nav-link"
-                            to="/leaderboard">
-                            Leaderboard
-                        </NavLink>
-                    </Nav>
-                    <Button variant="outline-success" onClick={this.handleSignOut}>Sign Out</Button>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+			<Navbar bg="light" expand="lg">
+				<Navbar.Brand>
+					<Image
+						src={user.avatarURL}
+						className="d-inline-block align-top"
+						style={{ height: '30px', marginRight: '8px'}} 
+						roundedCircle />
+						{`${user.name}`}
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<NavLink
+							exact
+							className="nav-link"
+							to="/">
+							Home
+						</NavLink>
+						<NavLink
+							className="nav-link"
+							to="/add">
+							New Question
+						</NavLink>
+						<NavLink 
+							className="nav-link"
+							to="/leaderboard">
+							Leaderboard
+						</NavLink>
+					</Nav>
+					<Button variant="outline-success" onClick={this.handleSignOut}>Sign Out</Button>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+	}
 }
 
 function mapStateToProps({ authedUser, users }) {
