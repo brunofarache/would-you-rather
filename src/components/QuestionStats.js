e8ef9e4a152efa736255f1527c68544e50e7c799
Bruno Farache <bruno.farache@liferay.com>
2019-05-04 11:48:15 -0300

    move initialization to constructor

--- src/components/QuestionStats.js
+++ src/components/QuestionStats.js
@@ -2,34 +2,40 @@ import React, { Component } from 'react';
 import { Badge, Card, ProgressBar } from 'react-bootstrap';
 
 class QuestionsStats extends Component {
-	render() {
-		const { authedUser, author, question: { optionOne, optionTwo } } = this.props;
+	constructor(props) {
+		super(props);
+
+		const { authedUser, question: { optionOne, optionTwo } } = props;
 		const oneVotes = optionOne.votes.length;
 		const twoVotes = optionTwo.votes.length;
-		const total = oneVotes + twoVotes;
-		const onePercentage = Math.floor((oneVotes / total) * 100);
-		const twoPercentage = Math.floor((twoVotes / total) * 100);
-		const votedOne = optionOne.votes.includes(authedUser);
-		const votedTwo = optionTwo.votes.includes(authedUser);
+
+		this.total = oneVotes + twoVotes;
+		this.onePercentage = Math.floor((oneVotes / this.total) * 100);
+		this.twoPercentage = Math.floor((twoVotes / this.total) * 100);
+		this.votedOne = optionOne.votes.includes(authedUser);
+		this.votedTwo = optionTwo.votes.includes(authedUser);
+	}
+	render() {
+		
 		
 		return (
 			<Card style={{ width: '18rem' }}>
-				<Card.Header>Asked by {author.name}</Card.Header>
+				<Card.Header>Asked by {this.props.author.name}</Card.Header>
 				<Card.Body>
 					<Card.Title>Results:</Card.Title>
 					<strong>Option 1:</strong><br />
-					{optionOne.text} {votedOne && <Badge variant="success">Your Vote</Badge>}
+					{this.props.question.optionOne.text} {this.votedOne && <Badge variant="success">Your Vote</Badge>}
 					<ProgressBar
 						style={{height: '35px'}}
-						now={onePercentage}
-						label={`${onePercentage}% (${oneVotes} out of ${total})`} />
+						now={this.onePercentage}
+						label={`${this.onePercentage}% (${this.oneVotes} out of ${this.total})`} />
 					<strong>Option 2:</strong><br />
-					{optionTwo.text} {votedTwo && <Badge variant="success">Your Vote</Badge>}
+					{this.props.question.optionTwo.text} {this.votedTwo && <Badge variant="success">Your Vote</Badge>}
 					<ProgressBar
 						style={{height: '35px'}}
-						now={twoPercentage}
+						now={this.twoPercentage}
 						variant="warning"
-						label={`${twoPercentage}% (${twoVotes} out of ${total})`} />
+						label={`${this.twoPercentage}% (${this.twoVotes} out of ${this.total})`} />
 				</Card.Body>
 			</Card>
 		);
